72338ebae72ca3c22d32600a6064e14e64c2eb7b
Patrick <haoqi.cui@gmail.com>
2023-08-23 16:04:03 -0700

    testing map

--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
                 {/*</span>*/}
                 <body>
                 <div>
-                    <Map></Map>
+                    {/* <Map></Map> */}
                 </div>
                 </body>
             </Row>
